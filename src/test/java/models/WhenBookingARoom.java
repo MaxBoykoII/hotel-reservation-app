5d2a4e56214924e4b7a9fd90cbea1d0126f1ba02
Max Boyko <maxboykomax@gmail.com>
2021-05-08 17:17:15 -0700

    Add Before hook

--- src/test/java/models/WhenBookingARoom.java
+++ src/test/java/models/WhenBookingARoom.java
@@ -1,14 +1,20 @@
 package models;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WhenBookingARoom {
 
+    private Room room;
+
+    @Before
+    public void setup() {
+        this.room = new Room(225.0, 5);
+    }
+
     @Test
     public void should_report_room_price() {
-        Room room = new Room(225.0, 5);
-
         double price = room.getRoomPrice();
 
         assertThat(price).isEqualTo(225.0);
@@ -16,8 +22,6 @@ public class WhenBookingARoom {
 
     @Test
     public void should_get_room_number() {
-        Room room = new Room(225.0, 5);
-
         int roomNumber = room.getRoomNumber();
 
         assertThat(roomNumber).isEqualTo(5);
