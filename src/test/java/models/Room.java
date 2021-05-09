622c620eed8f41cfd4f7939aaf7b7e6be1f1c7bc
Max Boyko <maxboykomax@gmail.com>
2021-05-08 17:13:58 -0700

    Add test for getting the room number

--- src/test/java/models/Room.java
+++ src/test/java/models/Room.java
@@ -3,11 +3,17 @@ package models;
 public class Room {
 
     private final double price;
+    private final int roomNumber;
 
-    public Room(double price)  {
+    public Room(double price, int roomNumber)  {
         this.price = price;
+        this.roomNumber = roomNumber;
     }
     public double getRoomPrice() {
         return this.price;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
     }
 }
--- src/test/java/models/WhenBookingARoom.java
+++ src/test/java/models/WhenBookingARoom.java
@@ -7,10 +7,19 @@ public class WhenBookingARoom {
 
     @Test
     public void should_report_room_price() {
-        Room room = new Room(225.0);
+        Room room = new Room(225.0, 5);
 
         double price = room.getRoomPrice();
 
         assertThat(price).isEqualTo(225.0);
+    }
+
+    @Test
+    public void should_get_room_number() {
+        Room room = new Room(225.0, 5);
+
+        int roomNumber = room.getRoomNumber();
+
+        assertThat(roomNumber).isEqualTo(5);
     }
 }
