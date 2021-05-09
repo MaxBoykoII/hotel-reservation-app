d8fcebc1e5ca458b545eef417a593a133137c852
Max Boyko <maxboykomax@gmail.com>
2021-05-08 17:24:52 -0700

    Refactor tests

--- src/test/java/models/WhenBookingARoom.java
+++ src/test/java/models/WhenBookingARoom.java
@@ -7,23 +7,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class WhenBookingARoom {
 
     private Room room;
+    private final double price = 225.0;
+    private final int roomNumber = 5;
 
     @Before
     public void setup() {
-        this.room = new Room(225.0, 5);
+        this.room = new Room(price, roomNumber);
     }
 
     @Test
     public void should_report_room_price() {
         double price = room.getRoomPrice();
 
-        assertThat(price).isEqualTo(225.0);
+        assertThat(price).isEqualTo(this.price);
     }
 
     @Test
     public void should_get_room_number() {
         int roomNumber = room.getRoomNumber();
 
-        assertThat(roomNumber).isEqualTo(5);
+        assertThat(roomNumber).isEqualTo(this.roomNumber);
     }
 }
