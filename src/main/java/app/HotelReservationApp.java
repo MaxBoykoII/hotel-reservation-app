4d88998c0abbb0b564fa61704ef40a0764d29e2a
Max Boyko <maxboykomax@gmail.com>
2021-05-09 20:35:46 -0700

    Begin fleshing out the steps for the first scenario

---
+++ src/main/java/app/HotelReservationApp.java
@@ -0,0 +1,6 @@
+package app;
+
+public class HotelReservationApp {
+    public static void main() {
+    }
+}
--- src/test/java/integration/stepdefinitions/MenuSteps.java
+++ src/test/java/integration/stepdefinitions/MenuSteps.java
@@ -1,19 +1,31 @@
 package integration.stepdefinitions;
 
+import app.HotelReservationApp;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class MenuSteps {
+    private ByteArrayOutputStream bytes;
     @Given("Rusty opens the hotel reservation application")
     public void rusty_opens_the_hotel_reservation_application() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        bytes = new ByteArrayOutputStream();
+        PrintStream console = System.out;
+
+        System.setOut(new PrintStream(bytes));
+        HotelReservationApp.main();
+
+        System.setOut(console);
     }
 
     @Then("Rusty will be welcomed")
     public void rusty_will_be_welcomed() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        assertThat(bytes.toString()).contains("Welcome to the Hotel Reservation Application");
     }
 
     @Then("Rusty will see a list of actions he can take")
