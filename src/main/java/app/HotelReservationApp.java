6f080d89a23e9b351c15ca05497649a5aca70bb7
Max Boyko <maxboykomax@gmail.com>
2021-05-09 20:42:20 -0700

    Display list of menus and implement corresponding step definition

--- src/main/java/app/HotelReservationApp.java
+++ src/main/java/app/HotelReservationApp.java
@@ -3,5 +3,10 @@ package app;
 public class HotelReservationApp {
     public static void main() {
         System.out.println("Welcome to the Hotel Reservation Application");
+        System.out.println("1. Find and reserve a room");
+        System.out.println("2. See my reservations");
+        System.out.println("3. Create an account");
+        System.out.println("4. Admin");
+        System.out.println("5. Exit");
     }
 }
--- src/test/java/integration/stepdefinitions/MenuSteps.java
+++ src/test/java/integration/stepdefinitions/MenuSteps.java
@@ -11,6 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class MenuSteps {
     private ByteArrayOutputStream bytes;
+
     @Given("Rusty opens the hotel reservation application")
     public void rusty_opens_the_hotel_reservation_application() {
         bytes = new ByteArrayOutputStream();
@@ -31,7 +32,12 @@ public class MenuSteps {
     @Then("Rusty will see a list of actions he can take")
     public void rusty_will_see_a_list_of_actions_he_can_take() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        assertThat(bytes.toString())
+                .contains("1. Find and reserve a room")
+                .contains("2. See my reservations")
+                .contains("3. Create an account")
+                .contains("4. Admin")
+                .contains("5. Exit");
     }
 
     @Then("Rusty will be prompted to select a menu option")
