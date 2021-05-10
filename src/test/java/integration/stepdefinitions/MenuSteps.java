016af3a142dc1f42a9888a3e1e66d9cf04dd5ad8
Max Boyko <maxboykomax@gmail.com>
2021-05-09 20:20:26 -0700

    Integrate Cucumber

---
+++ src/test/java/integration/stepdefinitions/MenuSteps.java
@@ -0,0 +1,30 @@
+package integration.stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class MenuSteps {
+    @Given("Rusty opens the hotel reservation application")
+    public void rusty_opens_the_hotel_reservation_application() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Rusty will be welcomed")
+    public void rusty_will_be_welcomed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Rusty will see a list of actions he can take")
+    public void rusty_will_see_a_list_of_actions_he_can_take() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Rusty will be prompted to select a menu option")
+    public void rusty_will_be_prompted_to_select_a_menu_option() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
---
+++ src/test/resources/features/menus.feature
@@ -0,0 +1,7 @@
+Feature: Menus
+
+  Scenario: Master Menu
+    Given Rusty opens the hotel reservation application
+    Then Rusty will be welcomed
+    And Rusty will see a list of actions he can take
+    And Rusty will be prompted to select a menu option
